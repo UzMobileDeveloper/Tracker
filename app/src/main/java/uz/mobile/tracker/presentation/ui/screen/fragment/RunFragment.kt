50a0582b3f64ed58d8c35819b69e4f30b93619c6
UzMobileDeveloper <abdumaliknuraliyev2002@gmail.com>
2023-09-07 10:32:28 +0500

    Permission implemented

--- app/src/main/java/uz/mobile/tracker/presentation/ui/screen/fragment/RunFragment.kt
+++ app/src/main/java/uz/mobile/tracker/presentation/ui/screen/fragment/RunFragment.kt
@@ -1,5 +1,7 @@
 package uz.mobile.tracker.presentation.ui.screen.fragment
 
+import android.Manifest
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,12 +10,15 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import dagger.hilt.android.AndroidEntryPoint
+import pub.devrel.easypermissions.AppSettingsDialog
+import pub.devrel.easypermissions.EasyPermissions
 import uz.mobile.tracker.R
 import uz.mobile.tracker.databinding.FragmentRunBinding
 import uz.mobile.tracker.presentation.viewModel.MainViewModel
+import uz.mobile.tracker.util.TrackingUtil
 
 @AndroidEntryPoint
-class RunFragment : Fragment(R.layout.fragment_run) {
+class RunFragment : Fragment(R.layout.fragment_run), EasyPermissions.PermissionCallbacks {
     private val viewModel: MainViewModel by viewModels()
     private lateinit var binding: FragmentRunBinding
     override fun onCreateView(
@@ -33,7 +38,51 @@ class RunFragment : Fragment(R.layout.fragment_run) {
                 findNavController().navigate(R.id.action_runFragment_to_trackingFragment)
             }
         }
+        requestPermissions()
+    }
+
+    private fun requestPermissions() {
+        if (TrackingUtil.hasLocationPermission(requireContext())) {
+            return
+        } else {
+            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) {
+                EasyPermissions.requestPermissions(
+                    this,
+                    "You need to accept location permission", 0,
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION,
+                )
+            } else {
+                EasyPermissions.requestPermissions(
+                    this,
+                    "You need to accept location permission", 0,
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION,
+                    Manifest.permission.ACCESS_BACKGROUND_LOCATION,
+                )
+            }
+
+        }
+    }
+
+    override fun onPermissionsGranted(requestCode: Int, perms: MutableList<String>) {
+    }
+
+    override fun onPermissionsDenied(requestCode: Int, perms: MutableList<String>) {
+        if (EasyPermissions.somePermissionPermanentlyDenied(this, perms)) {
+            AppSettingsDialog.Builder(this).build().show()
+        } else {
+            requestPermissions()
+        }
 
     }
 
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this)
+    }
 }
